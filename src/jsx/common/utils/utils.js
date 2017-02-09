927bc906b3aac696f6ee280d4b4845543d4978f4
i5on9i <gaedduck@gmail.com>
2017-02-09 22:13:48 +0900

    - utils/ajaxSend

---
+++ src/jsx/common/utils/utils.js
@@ -0,0 +1,31 @@
+
+
+export const ajaxSend = (options)=>{
+    var {type, url, success, error, dataType} = options
+    
+    var xhr = new XMLHttpRequest();
+	
+    xhr.open(type, encodeURI(url));
+	xhr.responseType = dataType;
+    
+    // User-Agent
+    // the below does not work, see background.js to change user-agent
+    // xhr.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36")
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            if(success !== 'undefined'){
+                success(xhr.responseText)    
+            }
+            
+        }
+        else {
+            if(error !== 'undefined'){
+                error(xhr, xhr.status)    
+            }
+        }
+        
+    };
+    xhr.send();
+ 
+}
